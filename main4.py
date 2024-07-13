70a4303c606b3dd924d75c9953343b181fc71c88
ahmadromadhani007 <dan10kiy@gmail.com>
2024-07-13 21:18:22 +0700

    update1 main4

---
+++ main4.py
@@ -0,0 +1,9 @@
+#2.4 Menampilkan Grafik a. perintah st.line()
+import streamlit as st
+import pandas as pd
+import numpy as np
+chart_data = pd.DataFrame(
+    np.random.randn(5, 3),
+    columns=['kolom1', 'kolom2', 'kolom3'])
+st.table(chart_data)
+st.line_chart(chart_data)
