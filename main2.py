f3003dd0e0c24c4c743e8ac2955a58498e44bb3f
ahmadromadhani007 <dan10kiy@gmail.com>
2024-07-13 20:16:47 +0700

    update main2

--- main2.py
+++ main2.py
@@ -3,3 +3,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+#a. perintah DataFrame
+st.title('Dataframe Basic')
+df = pd.DataFrame(
+    np.random.rand(10, 5),
+    columns=('col %d' % i for i in range(5)))
+st.dataframe(df)
+#b. perintah st.table()
+st.table(df)
+
+
---
+++ main3.py
@@ -0,0 +1,3 @@
+#c. file metric.py()
+import streamlit as st
+st.metric(label="Temperature", value="70 oF", delta="1.2 oF")
