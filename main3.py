aaa618d35a26c7234d3a8450f21e041f9cd4bb61
ahmadromadhani007 <dan10kiy@gmail.com>
2024-07-13 20:28:57 +0700

    update main3

--- main3.py
+++ main3.py
@@ -1,3 +1,20 @@
 #c. file metric.py()
 import streamlit as st
 st.metric(label="Temperature", value="70 oF", delta="1.2 oF")
+st.caption('dibawah ini kombinasi coding st.metric() dengan st.column()')
+st.divider()
+kolom1, kolom2, kolom3 = st.columns(3)
+kolom1.metric("Temperature", "70 oF", "1.2 oF")
+kolom2.metric("Wind", "9 mph", "-8%")
+kolom3.metric("Humanity", "86%", "4%")
+st.divider()
+#d. perintah st.json()
+st.json({
+    'Name' : 'Ayu Nawiroh',
+    'Address' : 'Bondowoso',
+    'Hobby': [
+        'Cooking',
+        'Reciting',
+        'Memorizing',
+    ],
+})
